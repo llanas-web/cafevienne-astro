aeeaa30a6ebfdb1fd3d58214307e5adee0b44dd2
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2024-04-15 15:35:41 +0200

    ➕ Adding tailwind plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import vercelServerless from "@astrojs/vercel/serverless";
 import vercelStatic from "@astrojs/vercel/static";
+import tailwind from "@astrojs/tailwind";
 
 const confProd = {
   output: "static",
   adapter: vercelStatic(),
 };
-
 const confPreview = {
   output: "server",
   adapter: vercelServerless(),
@@ -16,5 +15,8 @@ const confPreview = {
 // https://astro.build/config
 export default defineConfig({
   ...(process.env.NODE_ENV === "production" ? confProd : confPreview),
-  devToolbar: { enabled: false },
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [tailwind()],
 });
