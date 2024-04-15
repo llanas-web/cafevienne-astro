864ccc1305a3f60ec7f213f92d9b58183389ff9f
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2024-04-15 15:34:39 +0200

    ➕ Adding vercel plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercelServerless from "@astrojs/vercel/serverless";
+import vercelStatic from "@astrojs/vercel/static";
+
+const confProd = {
+  output: "static",
+  adapter: vercelStatic(),
+};
+
+const confPreview = {
+  output: "server",
+  adapter: vercelServerless(),
+};
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  ...(process.env.NODE_ENV === "production" ? confProd : confPreview),
+  devToolbar: { enabled: false },
+});
