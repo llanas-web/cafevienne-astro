1365ecb55430ac7f7c7632204cc19be0d39b663b
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2024-04-15 19:48:02 +0200

    ✨ Creating lang download menu

--- src/api/strapi.ts
+++ src/api/strapi.ts
@@ -18,56 +18,15 @@ const fetchStrapi = async <T>(api: string, query: string = "") => {
 };
 
 export const useStrapi = () => {
-  const getCarte = async (local?: string) => {
-    const qsString = qs.stringify({});
-    const carte = await fetchStrapi("carte", qs.stringify(POPULATED_QUERY));
-    return carte.attributes;
-  };
-
-  const getHome = async () => {
+  const getCarte = async (locale = "all") => {
     const qsString = qs.stringify({
       populate: {
-        bio: {
-          populate: "image.formats",
-        },
-        seo: {
-          populate: "cover.formats",
-        },
-        cover: {
-          populate: "*",
-        },
-        actuality: {
-          populate: "*",
-        },
+        menu: "url",
       },
+      locale,
     });
-    const home = await fetchStrapi<Home>("home", qsString);
-    return home.attributes;
+    return fetchStrapi("carte", qsString);
   };
 
-  const getAgenda = async () => {
-    const qsString = qs.stringify({
-      populate: {
-        seo: {
-          populate: "cover.formats",
-        },
-      },
-    });
-    const agenda = await fetchStrapi<any>("agenda", qsString);
-    return agenda.attributes;
-  };
-
-  const getEvents = async () => {
-    const qsString = qs.stringify({
-      pagination: {
-        pageSize: 50,
-      },
-      populate: "image.formats",
-      sort: ["starting"],
-    });
-    const events = await fetchStrapi<Event[]>("events", qsString);
-    return events;
-  };
-
-  return { getHome, getAgenda, getEvents };
+  return { getCarte };
 };
