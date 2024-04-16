fa3ea840f9b31381d8264fb57742b885f4c98e55
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2024-04-16 14:51:29 +0200

    💄 Adding default colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,20 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
       title: ["Poppins", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        dark: colors.slate["800"],
+        bright: colors.slate["200"],
+        primary: colors.emerald["950"],
+        secondary: colors.amber["500"],
+      },
+    },
   },
   plugins: [],
 };
