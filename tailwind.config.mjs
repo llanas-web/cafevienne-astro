fcc6a2c9c1e4de3c332093331f83228e5b454223
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2024-04-19 10:30:25 +0200

    💄 ADding font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,8 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      title: ["Poppins", "sans-serif"],
+      bodoni: ["BodoniModa", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
     },
     extend: {
       colors: {
